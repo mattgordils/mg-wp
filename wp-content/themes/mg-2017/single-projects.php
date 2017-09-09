9fab90b7186aacb411470eaacb449d077723d864
Matt Gordils <mattgordils@gmail.com>
2017-09-09 10:23:39 -0400

    new design

--- wp-content/themes/mg-2017/single-projects.php
+++ wp-content/themes/mg-2017/single-projects.php
@@ -2,9 +2,9 @@
 	$templates = 'project.twig';
 	$context = Timber::get_context();
 	$context['post'] = new TimberPost();
-	$context['services'] = new TimberTerm();
+	// $context['services'] = new TimberTerm();
 	$post_id = get_the_ID();
-	$context['services'] = new TimberTerm();
+	$context['services'] = new TimberTerm('custom_cats');
 	$query = array(
 		'post_type' => 'projects',
 		'posts_per_page' => -1,
